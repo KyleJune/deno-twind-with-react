ee4e77de06f2a1cb374e36bc2bf918bd1a36eb57
Kyle June <github@kylejune.com>
2022-12-25 19:52:44 -0600

    Log tw object to console

--- client/twind.ts
+++ client/twind.ts
@@ -3,8 +3,10 @@ import {
   presetAutoprefix,
   presetTailwind,
 } from "../deps_client.ts";
-import { isProduction } from "../env.ts";
+import { isBrowser, isProduction } from "../env.ts";
 
 export const tw = installTwind({
   presets: [presetAutoprefix(), presetTailwind()],
 }, isProduction());
+
+if (isBrowser) console.log("tw", tw);
